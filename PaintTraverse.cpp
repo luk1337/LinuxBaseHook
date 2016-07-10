c4da19b07c0b194b36f7c4a48fab2e0550d0b57e
LuK1337 <priv.luk@gmail.com>
2016-07-10 20:54:47 +0200

    Readd watermark

--- PaintTraverse.cpp
+++ PaintTraverse.cpp
@@ -1,22 +1,24 @@
 #include "Hooks.h"
 #include "EntityESP.h"
+#include "Draw.h"
 
 PaintTraverseFn oPaintTraverse;
 
 void Hooks::PaintTraverse(void* v1, unsigned long long vguiPanel, bool forceRepaint, bool allowForce) {
     oPaintTraverse(v1, vguiPanel, forceRepaint, allowForce);
+
     const char* name = g_pPanel->GetName(vguiPanel);
-
-    if (name && name[0] == 'F' && name[5] == 'O' && name[12] == 'P') {
+    if (name && name[0] == 'M' && name[2] == 't') {
         static bool bDidOnce = false;
-
         if(!bDidOnce) {
             g_Draw.InitFont(12, "Tahoma");
             bDidOnce = true;
         }
 
-        CBaseEntity* pLocal = g_pEntityList->GetClientEntity(g_pEngine->GetLocalPlayer());
+        // Draw watermark
+        g_Draw.DrawString(false, 5, 5, 0, 0, 255, 255, "Basehook :3");
 
+        CBaseEntity* pLocal = g_pEntityList->GetClientEntity(g_pEngine->GetLocalPlayer());
         if (!pLocal) return;
 
         for(int i = 0; i < 64; ++i) {
